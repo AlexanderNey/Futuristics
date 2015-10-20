99b974552fac7e363ed084cf575ba039a7d0fae1
Alexander Ney <alexander.ney@me.com>
2015-10-20 16:02:55 +0100

    refactored project file, targets & build settings to new library name `Futuristics`

---
+++ Source/Futuristics.h
@@ -0,0 +1,19 @@
+//
+//  Futuristics.h
+//  Futuristics
+//
+//  Created by Alexander Ney on 03/08/2015.
+//  Copyright (c) 2015 Alexander Ney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Futuristics.
+FOUNDATION_EXPORT double FuturisticsVersionNumber;
+
+//! Project version string for Futuristics.
+FOUNDATION_EXPORT const unsigned char FuturisticsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Futuristics/PublicHeader.h>
+
+
