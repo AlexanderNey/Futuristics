44a8e0325c66dd7909f7d18cdec1a3fd8e8505e7
Alexander Ney <alexander.ney@me.com>
2015-11-20 15:21:14 +0000

    - added slimmer `onQueue` version to be used without delay parameter - await can't be executed on the mainthread anymore as it will block it - throw fatal error if attempt

--- Source/Asynchronous.swift
+++ Source/Asynchronous.swift
@@ -25,6 +25,10 @@ public func onBackgroundQueue<T, U>(after delay: Double? = nil)(_ closure: T thr
     return onQueue(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), after: delay)(closure)
 }
 
+public func onQueue<T, U>(queue: dispatch_queue_t, closure: T throws -> U) -> (T -> Future<U>) {
+    return onQueue(queue, after: nil)(closure)
+}
+
 public func onQueue<T, U>(queue: dispatch_queue_t, after delay: Double? = nil)(_ closure: T throws -> U) -> (T -> Future<U>) {
     return { (parameter: T) in
         let promise = Promise<U>()
@@ -54,6 +58,9 @@ public func await<T>(futures: Future<T> ...) {
 }
 
 public func await<T>(futures: [Future<T>]) {
+    guard !NSThread.isMainThread() else {
+        fatalError("await will block main thread")
+    }
     let semaphore = dispatch_semaphore_create(0)
     
     futures.forEach { future in
@@ -66,33 +73,3 @@ public func await<T>(futures: [Future<T>]) {
         dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER)
     }
 }
-
-/*
-public func bundle(futures: Future<Any> ...) -> Future<[Any]> {
-    let b = onBackgroundQueue() { () -> [Any] in
-        await(futures)
-        let states = futures.map { $0.state }
-        
-        let promiseResults: [Any] = states.flatMap { state in
-            if case .Fulfilled(let value) = state {
-                return value
-            }
-            return nil
-        }
-        
-        if promiseResults.count == futures.count {
-            return promiseResults
-        } else {
-            // Throw the first error that is found
-            for future in futures {
-                if case .Rejected(let error) = future.state {
-                    throw error
-                }
-            }
-        }
-        
-        return []
-    }
-
-    return b()
-}*/
