f9d2ceeb40ff398f1b6f6d95deffcfc3882e3469
Alexander Ney <alexander.ney@agoda.com>
2017-07-16 17:48:55 +0700

    refactored asynch tests to be more streamlined and use new await methods

--- Tests/AsynchronousTests.swift
+++ Tests/AsynchronousTests.swift
@@ -27,13 +27,11 @@ class AsynchronousTests : XCTestCase {
         let expectExecutionOnMainThread = expectation(description: "runs on main thread")
 
         let somefunction = DispatchQueue.main.futureAsync { (_: Void) -> Void in
-            if Thread.isMainThread {
-                expectExecutionOnMainThread.fulfill()
-            }
+            XCTAssertTrue(Thread.isMainThread)
+            expectExecutionOnMainThread.fulfill()
         }
 
         _ = somefunction(())
-        
         waitForExpectationsWithDefaultTimeout()
     }
     
@@ -43,9 +41,8 @@ class AsynchronousTests : XCTestCase {
         var imediateExecution = false
         let somefunction = DispatchQueue.main.futureSync { (_: Void) -> Void in
             imediateExecution = true
-            if Thread.isMainThread {
-                expectExecutionOnMainThread.fulfill()
-            }
+            XCTAssertTrue(Thread.current.isMainThread)
+            expectExecutionOnMainThread.fulfill()
         }
         
         _ = somefunction(())
@@ -58,9 +55,8 @@ class AsynchronousTests : XCTestCase {
         let expectExecutionOnBackgroundQueue = expectation(description: "runs on background queue")
 
         let somefunction = DispatchQueue.global(qos: .userInitiated).futureAsync { (_: Void) -> Void in
-            if !Thread.isMainThread {
-                expectExecutionOnBackgroundQueue.fulfill()
-            }
+            XCTAssertFalse(Thread.current.isMainThread)
+            expectExecutionOnBackgroundQueue.fulfill()
         }
         
         DispatchQueue.main.async {
@@ -78,17 +74,34 @@ class AsynchronousTests : XCTestCase {
         
         let somefunction = queue.futureAsync { () -> Void in
             let queueLabel = String(validatingUTF8: __dispatch_queue_get_label(nil))
-            if queueLabel == "testQueue"  {
-                expectExecutionOnCustomQueue.fulfill()
-            }
+            XCTAssertEqual(queueLabel, "testQueue")
+            expectExecutionOnCustomQueue.fulfill()
         }
         
         _ = somefunction(())
-        
+        waitForExpectationsWithDefaultTimeout()
+    }
+
+    func testSynchOnCustomQueue() {
+
+        let queue = DispatchQueue(label: "testQueue", attributes: [])
+
+        let expectExecutionOnCustomQueue = expectation(description: "runs on background queue")
+        var immediateExecution = false
+        let somefunction = queue.futureSync { () -> Void in
+            let queueLabel = String(validatingUTF8: __dispatch_queue_get_label(nil))
+            immediateExecution = true
+            XCTAssertEqual(queueLabel, "testQueue")
+            expectExecutionOnCustomQueue.fulfill()
+        }
+
+        _ = somefunction(())
+        XCTAssertTrue(immediateExecution)
         waitForExpectationsWithDefaultTimeout()
     }
     
     func testAwaitSinglePromiseImediateResult() {
+
         let somefunction = { () -> Future<String> in
             let promise = Promise<String>()
             promise.fulfill("done")
@@ -99,7 +112,8 @@ class AsynchronousTests : XCTestCase {
         let awaitExpectation = expectation(description: "await on some background queue")
         
         DispatchQueue.global(qos: .userInitiated).async {
-            await(future)
+            let result = try! future.await() as String
+            XCTAssertEqual(result, "done")
             awaitExpectation.fulfill()
         }
         
@@ -118,26 +132,21 @@ class AsynchronousTests : XCTestCase {
             promise.fulfill("done")
             return promise.future
         }
-        
     
-        let promiseA = someFunction()
-        let promiseB = someFunction()
+        let futureA = someFunction()
+        let futureB = someFunction()
         
         let awaitExpectation = expectation(description: "await on some background queue")
         
         DispatchQueue.global(qos: .userInitiated).async {
-            await(promiseA, promiseB)
+            _ = try? futureA.await() as String
+            _ = try? futureB.await() as String
+
+            XCTAssertTrue(futureA.state.isFulfilled)
+            XCTAssertTrue(futureB.state.isFulfilled)
             awaitExpectation.fulfill()
         }
         
         waitForExpectationsWithDefaultTimeout()
-        
-        switch (promiseA.state, promiseB.state) {
-        case (.fulfilled(let valueA), .fulfilled(let valueB)):
-            XCTAssertEqual(valueA, "done")
-            XCTAssertEqual(valueB, "done")
-        default:
-            XCTFail()
-        }
     }
 }
